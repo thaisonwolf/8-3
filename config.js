c01378174b5ee9e1eb6d37bec3069fddec79d877
thaisonwolf <thaison27052008@gmail.com>
2024-03-08 22:51:43 +0700

    pahoang

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Vật thể lạ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100043780058249';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng vật thể lạ';
+const contentCard = 'Chúc "vật thể lạ" của mình 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bn trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girlk8.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
